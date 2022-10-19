72a7355bfda3e6226238100f7d53afb0a458933c
Victor Adesoye <victoralphaferanmi01@gmail.com>
2022-10-18 17:28:42 -0700

    prints 10 times the alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,6 +3,7 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  *
+ * Return: 0
 */
 
 void print_alphabet_x10(void)
@@ -14,9 +15,7 @@ void print_alphabet_x10(void)
 	{
 
 		for (letter = 'a'; letter <= 'z'; letter++)
-		{
 			_putchar(letter);
-		}
 
 		_putchar('/n');
 	}
