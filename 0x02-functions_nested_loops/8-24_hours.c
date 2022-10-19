7db34def00a9e48f2e732ca7c6d2a4b2df0c471f
Victor Adesoye <victoralphaferanmi01@gmail.com>
2022-10-19 16:34:37 -0700

    prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 	{
 		while (minute++ <= 59)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
 			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
 			_putchar('\n');
 		}
 	}
